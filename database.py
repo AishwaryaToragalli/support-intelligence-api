612224fd3e0f3ce28aa011267a6da6a5f299ddd8
Aishwarya Toragalli <aishwaray7969@gmail.com>
2026-08-17 17:50:40 +0530

    Restore get_db context generator helper function

--- database.py
+++ database.py
@@ -1,31 +1,24 @@
 import os
-
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+if "pytest" in os.getenv("PYTEST_CURRENT_TEST",""):
+    DATABASE_URL = "sqlite:///:memory:"
+else:
+    DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(
-    DATABASE_URL,
-    pool_pre_ping=True
-)
+if DATABASE_URL and DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(DATABASE_URL,connect_args={"check_same_thread":False})
+else:
+    engine = create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
-
+SessionLocal = sessionmaker(autocommit=False, autoflush= False, bind=engine)
 Base = declarative_base()
-
-
 def get_db():
     db = SessionLocal()
-
     try:
         yield db
     finally:
